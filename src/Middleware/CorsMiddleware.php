84b5e54f4e144e30c7f3385d7fa97b3fa6a5d18d
nikolas-gou <nikolasgou23@gmail.com>
2025-03-07 15:08:58 +0200

    Changing on corsMiddlware localhost to *

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -14,7 +14,7 @@ class CorsMiddleware implements MiddlewareInterface
         $response = $handler->handle($request);
 
         return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
