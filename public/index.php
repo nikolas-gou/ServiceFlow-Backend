bccb0ceda3dd30bce91198e2eb7d310666b49f48
nikolas-gou <nikolasgou23@gmail.com>
2025-03-09 22:49:33 +0200

    Removing useless libraries

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 use Slim\Factory\AppFactory;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use App\Middleware\CorsMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
