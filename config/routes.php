d290b5dd95954261588a618de9bdea97aca61efa
nikolas-gou <nikolasgou23@gmail.com>
2025-03-09 18:20:06 +0200

    Adding motor routes

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,7 @@
 <?php
 use Slim\Routing\RouteCollectorProxy;
 use App\Controllers\CustomerController;
+use App\Controllers\MotorController;
 use App\Controllers\StatsOfCustomerController;
 
 $app->options('/{routes:.+}', function ($request, $response) {
@@ -11,6 +12,11 @@ $app->group('/api', function (RouteCollectorProxy $group) {
     $group->get('/customers', [CustomerController::class, 'getAll']);
     $group->post('/customers', [CustomerController::class, 'createCustomer']);
     $group->get('/customers/{id}', [CustomerController::class, 'getCustomerById']);
+
+    // Motors
+    $group->get('/motors', [MotorController::class, 'getAll']);
+    $group->post('/motors', [MotorController::class, 'createMotor']);
+    $group->get('/motors/{id}', [MotorController::class, 'getMotorById']);
 
     // Stats
     $group->get('/statsOfCustomers', [StatsOfCustomerController::class, 'getStats']);
