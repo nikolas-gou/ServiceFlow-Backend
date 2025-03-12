418dc6108495ca7ab579ae749764d14991fe88da
nikolas-gou <nikolasgou23@gmail.com>
2025-03-12 22:16:43 +0200

    Creating a new form for create a repair

--- src/Models/Repair.php
+++ src/Models/Repair.php
@@ -6,6 +6,8 @@ class Repair {
     public $motorID;
     public $customerID;
     public $repair_status;
+    public $description;
+    public $cost;
     public $created_at;
     public $customer;
     public $motor;
@@ -16,6 +18,8 @@ class Repair {
         $this->customerID = $data['customerID'] ?? '';
         $this->repair_status = $data['repair_status'] ?? '';
         $this->created_at = $data['created_at'] ?? null;
+        $this->description = $data['description'] ?? '';
+        $this->cost = $data['cost'] ?? '';
     }
 
     // public function isValid(): bool {
@@ -29,6 +33,8 @@ class Repair {
             'customerID' => $this->customerID,
             'repair_status' => $this->repair_status,
             'created_at' => $this->created_at,
+            'description' => $this->description,
+            'cost' => $this->cost,
             'customer' => $this->customer ? $this->customer->toArray() : null,
             'motor' => $this->motor ? $this->motor->toArray() : null
         ];
