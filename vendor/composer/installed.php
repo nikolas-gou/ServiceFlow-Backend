8a3942c17affcc545288adcd21db18236a28c411
nikolas-gou <nikolasgou23@gmail.com>
2025-05-24 11:30:26 +0300

    installed php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2a8dbea11dcf7e8729fed8587b0c57d16c944634',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2a8dbea11dcf7e8729fed8587b0c57d16c944634',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
