e24c1c12b627f89d137cd5fde38962ba92ed6add
nikolas-gou <nikolasgou23@gmail.com>
2025-06-12 21:44:40 +0300

    src/Models/Repair_Fault_Links.php

---
+++ src/Models/Repair_Fault_Links.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+class Repair_Fault_Links
+{
+    public $repair_id;
+    public $common_fault_id;
+
+
+    public function __construct(array $data = [])
+    {
+        $this->repair_id = $data['repair_id'] ?? '';
+        $this->common_fault_id = $data['common_fault_id'] ?? '';
+    }
+
+    public static function fromFrontendFormat(array $frontendData): self
+    {
+        $dbData = [
+            'repair_id' => $frontendData['repairID'] ?? null,
+            'common_fault_id' => $frontendData['commonFaultID'] ?? '',
+        ];
+
+        return new self($dbData);
+    }
+
+    public function toFrontendFormat(): array
+    {
+        return [
+            'repairID' => $this->repair_id,
+            'commonFaultID' => $this->common_fault_id,
+        ];
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'repair_id' => $this->repair_id,
+            'common_fault_id' => $this->common_fault_id,
+        ];
+    }
+}
